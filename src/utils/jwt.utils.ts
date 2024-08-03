e6678035c5a8fbbd43865ea6a08f7cdda8738df4
Basma Sabry <basmasabry33333@gmail.com>
2024-08-03 18:33:11 +0300

    feat(utils): update JWT and email utility functions

---
+++ src/utils/jwt.utils.ts
@@ -0,0 +1,19 @@
+import jwt from 'jsonwebtoken';
+import dotenv from "dotenv";
+dotenv.config();
+import config from "config";
+
+const publicKey :string= config.get("publicKey");
+const privateKey : string= config.get("privateKey");
+
+export const generateToken=(userId: string)=>
+{  return jwt.sign({ user: { id: userId } }, privateKey, { algorithm: 'RS256', expiresIn: process.env.ACCESS_TOKEN_TTL});
+}
+
+export const verifyToken=(token: string)=>{
+    return jwt.verify(token, publicKey, { algorithms: ['RS256'] });
+}
+
+export const generateRefreshToken=(userId: string)=>{
+    return jwt.sign({ user: { id: userId } }, privateKey, { algorithm: 'RS256', expiresIn: process.env.REFRESH_TOKEN_TTL});
+}
